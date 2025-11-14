a20129b8c080893bc871dc15788f6c993cea7ea7
luy869 <fyu.869000@gmail.com>
2025-11-15 05:30:19 +0900

    fix: DBとのやり取りをmodelへ移行

--- fuel/app/classes/model/userservice.php
+++ fuel/app/classes/model/userservice.php
@@ -1,29 +1,5 @@
 <?php
-
 namespace Model;
-
-class User extends \Orm\Model
-{
-    protected static $_properties = array(
-        'id',
-        'username',
-        'email',
-        'password',
-        'created_at',
-    );
-
-    protected static $_table_name = 'users';
-
-    // レポートとのリレーション
-    protected static $_has_many = array(
-        'reports' => array(
-            'key_from' => 'id',
-            'model_to' => 'Model\\Report',
-            'key_to' => 'user_id',
-        )
-    );
-}
-
 /**
  * ユーザー関連のDB操作をまとめた静的メソッド群
  * @package    Outdoor_Report
